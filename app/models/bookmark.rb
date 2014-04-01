7f8db2db9b121f81195f48b069ffcdb19f3e4a62
Miha Rekar <info@mr.si>
2014-04-01 22:26:22 +0200

    shorten url on bookmark save
    this should be run in background but for now it will do

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,12 +1,20 @@
+require 'open-uri'
 class Bookmark < ActiveRecord::Base
+  TINY_URL_API = 'http://tinyurl.com/api-create.php?url='
+
   belongs_to :site
   validates :url, presence: true
   acts_as_taggable
 
   before_save :set_site
+  before_save :shorten_url # This should be run in background
 
   def set_site
     self.site = Site.find_or_create_by(domain: domain)
+  end
+
+  def shorten_url
+    self.short_url = open(TINY_URL_API + url).read
   end
 
   private
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Bookmark do
   context '#create' do
+    subject { create(:bootstrap_buttons) }
     it 'creates new Site when saved' do
       expect(Site.count).to eq(0)
       create(:bootstrap_buttons)
@@ -17,6 +18,10 @@ describe Bookmark do
       create(:bootstrap_images)
       expect(Site.first.domain).to eq('getbootstrap.com')
       expect(Site.count).to eq(1)
+    end
+
+    it 'shortens the URL' do
+      expect(subject.short_url).not_to be_empty
     end
   end
 end
