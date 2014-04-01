ce79bc200b0beee4cc3260951b93730c629bb09b
Miha Rekar <info@mr.si>
2014-04-01 21:20:09 +0200

    create bookmark and redirect back to root if empty URL

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,15 @@
 class BookmarksController < ApplicationController
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      raise @bookmark
+    else
+      redirect_to :root, alert: "URL can't be blank"
+    end
+  end
+
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:url, :tag_list)
   end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,5 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :site
+  validates :url, presence: true
   acts_as_taggable
 end
